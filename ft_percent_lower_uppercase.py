5eba3f2ac9192e00c21ca25c57dabdd711174fd1
easykatka04 <71816857+easykatka04@users.noreply.github.com>
2020-10-01 20:51:57 +0300

    Create ft_percent_lower_uppercase.py

---
+++ ft_percent_lower_uppercase.py
@@ -0,0 +1,13 @@
+UPPERCASE = 'QWERTYUIOPASDFGHJKLZXCVBNMЁЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ'
+LOWERCASE = 'qwertyuiopasdfghjklzxcvbnmёйцукенгшщзхъфывапролджэячсмитьбю'
+
+
+def ft_percent_lower_uppercase(a):
+    count_upp = 0
+    count_low = 0
+    for char in a:
+        if char in UPPERCASE:
+            count_upp += 1
+        elif char in LOWERCASE:
+            count_low += 1
+    return count_lowercase / count_uppercase
